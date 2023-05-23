0cfdd16f696010c9e12367d94a522533d45a653c
Alemba-Vivian <alembavivian@gmail.com>
2023-05-23 21:57:44 +0300

    Added hover effects on the divs

--- js/main.js
+++ js/main.js
@@ -1,12 +1,18 @@
 let gridContainer = document.querySelector('.grid-container');
 let promptMessage = document.getElementById('popup');
 let validity = document.getElementById('validity');
+let buttonReset = document.getElementById('reset');
+//default color
+let color ='black';
 
 //Default grid
 createGrid(16);
 
+//reset function
+buttonReset.addEventListener('click', resetDivs);
+
 //when the user clicks the button, a prompt message is shown
-promptMessage.addEventListener('click', () =>{
+promptMessage.addEventListener('click', ()=>{
     let size = createPrompt();
     createGrid(size);
 });
@@ -25,11 +31,7 @@ function createPrompt(){
         validity.textContent=`Draw now `;
         return userInput;
     }
-  
-    
-}
-
-
+  }
 
 //creating a function that creates the grid squares
 function createGrid(size){
@@ -43,7 +45,35 @@ for(let i =0;i<numDivs; i++){
     //create new div
     let div = document.createElement('div');
     div.classList.add('grid-item');
-    div.style.backgroundColor ='red';
+    div.addEventListener('mouseover', colorChange)
     gridContainer.insertAdjacentElement('beforeend', div);
 }
+}
+
+function colorChange(){
+    if(color ==='random'){
+        let red = Math.floor(Math.random() * 256);
+        let green = Math.floor(Math.random() * 256);
+        let blue = Math.floor(Math.random() * 256);
+        let newColor = "rgb(" + red + "," + green + "," + blue + ")";
+        this.style.backgroundColor = newColor;
+    }else if(color ==='red'){
+        this.style.backgroundColor = 'red';
+    }else if(color ==='blue'){
+        this.style.backgroundColor ='blue';
+    }else{
+        this.style.backgroundColor = 'black';
+    }
+
+}
+function setColor(colorChoice){
+    color = colorChoice;
+
+}
+function resetDivs(){
+    let divs = document.querySelectorAll('div');
+    divs.forEach(div=>{
+        div.style.backgroundColor = '';
+    })
+
 }
